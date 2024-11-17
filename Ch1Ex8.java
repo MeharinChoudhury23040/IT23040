f5a7f1f8a95b037d562c006838c6d1c443bbbbdd
MeharinChoudhury23040 <it23040@mbstu.ac.bd>
2024-11-17 23:44:18 +0600

    Add files via upload

---
+++ Ch1Ex8.java
@@ -0,0 +1,12 @@
+public class Ch1Ex8
+{
+    public static void main(String[] args) 
+    {
+       double radius=5.5;
+       double pi=Math.PI;
+       double perimeter=2*radius*pi;
+       double area=radius*radius*pi;  
+       System.out.println("Perimeter of Radius "+radius+" is= "+perimeter);
+       System.out.println("Perimeter of Radius "+radius+" is= "+area);
+    }
+}
