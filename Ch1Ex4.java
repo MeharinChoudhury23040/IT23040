bfd24f9344b1ed758f3447bb398a1a465f45f957
MeharinChoudhury23040 <it23040@mbstu.ac.bd>
2024-11-17 23:09:49 +0600

    Add files via upload

---
+++ Ch1Ex4.java
@@ -0,0 +1,14 @@
+public class Ch1Ex4
+{
+    public static void main(String[] args) 
+    {
+	System.out.println("a	a^2	a^3");
+        for (int i = 1; i < 5; i++) 
+        {
+            int a1=i;
+            int a2=i*i;
+            int a3=i*i*i;
+            System.out.println(a1+"	"+a2+"	"+a3);
+        }
+    }
+}
