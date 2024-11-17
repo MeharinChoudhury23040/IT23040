f89211feb0fb2478583c0811e60fc57ccfa767cc
MeharinChoudhury23040 <it23040@mbstu.ac.bd>
2024-11-17 23:36:00 +0600

    Add files via upload

---
+++ Ch1Ex7.java
@@ -0,0 +1,10 @@
+public class Ch1Ex7
+{
+    public static void main(String[] args) 
+    {
+       double pi1=4.0*(1.0-(1.0/3.0)+(1.0/5.0)-(1.0/7.0)+(1.0/9.0)-(1.0/11.0)); 
+       double pi2=4.0*(1.0-(1.0/3.0)+(1.0/5.0)-(1.0/7.0)+(1.0/9.0)-(1.0/11.0)+(1.0/13.0));  
+       System.out.println("1st approximation of pi: "+pi1);
+       System.out.println("2nd approximation of pi: "+pi2);
+    }
+}
